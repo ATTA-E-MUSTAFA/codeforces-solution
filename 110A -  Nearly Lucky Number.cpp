5e86af21552e4617a9b0316a3052916c651a27b3
ATTA-E-MUSTAFA <attaemustafa442@gmail.com>
2023-01-28 09:31:25 +0500

    Submitted by Atta-e-mustafa

---
+++ 110A -  Nearly Lucky Number.cpp
@@ -0,0 +1,44 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+long long num;
+
+cin>>num;
+
+long long count_num=0;
+
+long long num2 = num;
+
+int rem=0;
+
+for(int i=0;num2>0;i++){
+
+rem = num2 % 10;
+
+if(rem==7||rem==4){
+
+     count_num++;
+}
+    num2=num2/10;
+
+
+}
+//cout<<count_num<<endl;
+
+if(count_num==7 || count_num==4){
+
+    printf("YES");
+
+}
+else{
+
+    printf("NO");
+
+}
+}
+
+
