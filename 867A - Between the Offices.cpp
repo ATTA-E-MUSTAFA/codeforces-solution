4aba4b5b893542df196ecaaf0d970aad566d9fbd
Your Name <you@example.com>
2023-03-13 22:24:10 +0500

    Between the offices.cpp

---
+++ 867A - Between the Offices.cpp
@@ -0,0 +1,29 @@
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//    int num;
+//    cin >> num;
+//
+//    string ch;
+//    cin >> ch;
+//    int stof = 0;
+//    int ftos = 0;
+//    for (int i = 0; ch[i + 1] != 0; i++) {
+//
+//        if (ch[i] == 'S' && ch[i + 1] == 'F') {
+//            stof++;
+//        }
+//        else if (ch[i] == 'F' && ch[i + 1] == 'S') {
+//            ftos++;
+//        }
+//    }
+//    if (stof > ftos) {
+//        printf("YES\n");
+//    }
+//    else {
+//        printf("NO\n");
+//    }
+//
+//}
