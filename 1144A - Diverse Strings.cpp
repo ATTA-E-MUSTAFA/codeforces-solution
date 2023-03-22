a7c82e9b43d69911dddde936d54f65bae3e7856d
Your Name <you@example.com>
2023-03-22 21:47:00 +0500

    Diverse Strings.cpp

---
+++ 1144A - Diverse Strings.cpp
@@ -0,0 +1,34 @@
+//
+//#include<iostream>
+//#include<algorithm>
+//#include<cmath>
+//using namespace std;
+//
+//
+//int main() {
+//	int test;
+//	cin >> test;
+//	while (test--) {
+//		string s;
+//		cin >> s;
+//		int len = s.length();
+//		sort(begin(s), end(s));
+//		//cout << s << endl;
+//		int flag = 0;
+//		for (int i = 0; i < s.length() - 1; i++) {
+//			if (s[i + 1] - 1 == s[i]) {
+//				continue;
+//			}
+//			else {
+//				flag = 1;
+//				break;
+//			}
+//		}
+//		if (flag == 0) {
+//			printf("YES\n");
+//		}
+//		else {
+//			printf("NO\n");
+//		}
+//	}
+//}
