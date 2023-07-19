1824d4fca9aa32c6429e63eb911d9926637c4a32
Your Name <you@example.com>
2023-07-19 23:24:19 +0500

    stock arbitraging.cpp

---
+++ 1150A-Stock Arbitraging.cpp
@@ -0,0 +1,35 @@
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int n, m, r;
+	cin >> n >> m >> r;
+	vector<int>buy;
+	vector<int>sell;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		buy.push_back(a);
+	}
+	for (int i = 0; i < m; i++) {
+		int a;
+		cin >> a;
+		sell.push_back(a);
+	}
+	sort(buy.begin(), buy.end());
+	sort(sell.begin(), sell.end());
+	int current = buy[0];
+	int maxSell = sell[m - 1];
+	current = r / current;
+	int rem = r - buy[0] * current;
+	rem += current * maxSell;
+	if (r < rem) {
+		cout << rem << endl;
+	}
+	else {
+		cout << r << endl;
+	}
+}
