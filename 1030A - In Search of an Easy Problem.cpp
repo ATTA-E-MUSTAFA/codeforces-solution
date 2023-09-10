1226dd4ef119fdbb82c30b9f3eaac7fde71b06bc
ATTA-E-MUSTAFA <102528299+ATTA-E-MUSTAFA@users.noreply.github.com>
2023-09-10 23:13:12 +0500

    Update 1030A - In Search of an Easy Problem.cpp

--- 1030A - In Search of an Easy Problem.cpp
+++ 1030A - In Search of an Easy Problem.cpp
@@ -6,7 +6,6 @@ cin>>numOfPeoples;
 int opinion=0;
 
 int count_0=0;          //counting all the easy answers
-
 int count_1=0;          //counting all the hard answers
 for(int i=0;i<numOfPeoples;i++){
 
